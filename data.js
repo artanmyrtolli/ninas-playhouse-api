a0c1704134e8f85c0c3c2675d6fa03eb49920827
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-06-09 02:46:05 -0400

    Add toy inventory

--- data.js
+++ data.js
@@ -2,15 +2,13 @@ let toyData = [
     {
         id: 1, 
         name:'TopBright Building Toys', 
-        type: 'blocks', 
         description: 'Colorful small blocks, comes in a bucket, organizational lid',
         ages: [2,6],
         img: 'https://m.media-amazon.com/images/I/71UbD9ap-ML._AC_SX679_.jpg'    
     },
     {
         id: 2, 
-        name:'Barbie Dreamtopia Rainbow Magic Mermaid', 
-        type: 'doll', 
+        name:'Barbie Dreamtopia Rainbow Magic Mermaid',  
         description: 'Mermaid barbie doll, no accessories',
         ages: '3+',
         img: 'https://m.media-amazon.com/images/I/61hWRyCZDQL._AC_SY879_.jpg'    
@@ -18,11 +16,32 @@ let toyData = [
     {
         id: 3, 
         name:'Hasbro Gaming CONNECT 4', 
-        type: 'Board Game', 
         description: "Classic Connect 4 game. For 2 players. Includes grid, 2 legs, slider bar, 21 red discs, 21 yellow discs and instructions",
         ages: '6+',
         img: 'https://m.media-amazon.com/images/I/81fEiLrmZ8L._AC_SX679_.jpg'    
+    },
+    {
+        id: 4, 
+        name:'Miniature Dyson Ball Replica For Children', 
+        description: "JUST LIKE THE REAL THING: From the twist and turn ball to the clear cylinder, this toy Dyson Ball looks and works just like the real thing",
+        ages: '3+',
+        img: 'https://m.media-amazon.com/images/I/51zL-lqq0sL._AC_SY879_.jpg'    
+    },
+    {
+        id: 5, 
+        name:'Manhattan Toy Winkel Rattle & Sensory Teether Toy', 
+        description: "BABY TEETHING TOY MADE FOR LITTLE HANDS",
+        ages: '0+',
+        img: 'https://m.media-amazon.com/images/I/71tjzN4QkCL._AC_SX679_.jpg'    
+    },
+    {
+        id: 6, 
+        name:'LeapFrog LeapLand Adventures', 
+        description: "This video game for early learners takes kids on a quest for keys to unlock Clever Castle while exploring letters, numbers, shapes and colors",
+        ages: '4+',
+        img: 'https://m.media-amazon.com/images/I/611qaAdlfZL._AC_SX679_.jpg'    
     }
+
   ]
 
   module.exports = toyData
