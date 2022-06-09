4d2865e44986003e561faf2ccf3dd9a13cfaa1af
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-06-09 02:39:50 -0400

    Add toy inventory

--- data.js
+++ data.js
@@ -1,7 +1,28 @@
 let toyData = [
-    {id: 1, title: 'Sweaters for pugs', description: 'To keep them warm'},
-    {id: 2, title: 'A romcom', description: 'But make it ghosts'},
-    {id: 3, title: 'A game show called Ether/Or', description: 'When you lose you get chloroformed'},
+    {
+        id: 1, 
+        name:'TopBright Building Toys', 
+        type: 'blocks', 
+        description: 'Colorful small blocks, comes in a bucket, organizational lid',
+        ages: [2,6],
+        img: 'https://m.media-amazon.com/images/I/71UbD9ap-ML._AC_SX679_.jpg'    
+    },
+    {
+        id: 2, 
+        name:'Barbie Dreamtopia Rainbow Magic Mermaid', 
+        type: 'doll', 
+        description: 'Mermaid barbie doll, no accessories',
+        ages: '3+',
+        img: 'https://m.media-amazon.com/images/I/61hWRyCZDQL._AC_SY879_.jpg'    
+    },
+    {
+        id: 3, 
+        name:'Hasbro Gaming CONNECT 4', 
+        type: 'Board Game', 
+        description: "Classic Connect 4 game. For 2 players. Includes grid, 2 legs, slider bar, 21 red discs, 21 yellow discs and instructions",
+        ages: '6+',
+        img: 'https://m.media-amazon.com/images/I/81fEiLrmZ8L._AC_SX679_.jpg'    
+    }
   ]
 
   module.exports = toyData
